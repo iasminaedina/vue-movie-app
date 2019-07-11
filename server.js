b3903d1ca5f7a5584a42da66ea57933c8decd974
iasminaedina <iasmina.edina@gmail.com>
2019-07-11 14:47:28 +0200

    possible fix for heroku

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 // server.js
 var express = require('express');
 var path = require('path');
+const history = require('connect-history-api-fallback');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+const staticFileMiddleware = express.static(__dirname);
+app.use(staticFileMiddleware);
+app.use(history({
+  disableDotRule: true,
+  verbose: true,
+}));
+app.use(staticFileMiddleware);
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + "/dist"), function (err) {
     if (err) {
--- tests/unit/moviesList.spec.js
+++ tests/unit/moviesList.spec.js
@@ -1,4 +1,4 @@
-
+import Vue from 'vue';
 import { expect } from 'chai';
 import MoviesList from '@/components/moviesList/moviesList.vue';
 
@@ -8,5 +8,24 @@ describe('MoviesList', () => {
   // Inspect the raw component options
   it('has a created hook', () => {
     expect(typeof MoviesList.created).toBe('function');
+  });
+
+  // helper function that mounts and returns the rendered text
+  function getRenderedText(Component, propsData) {
+    const Constructor = Vue.extend(Component);
+    const vm = new Constructor({ propsData }).$mount();
+    return vm.$el.textContent;
+  }
+
+  describe('MoviesList', () => {
+    it('renders correctly with different props', () => {
+      expect(getRenderedText(MoviesList, {
+        msg: 'Hello',
+      })).toBe('Hello');
+
+      expect(getRenderedText(MoviesList, {
+        msg: 'Bye',
+      })).toBe('Bye');
+    });
   });
 });
