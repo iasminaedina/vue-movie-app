94e7a9f967b1b66e2723bc43ad955924cf87f493
iasminaedina <iasmina.edina@gmail.com>
2019-07-11 15:02:30 +0200

    fix for redirect

--- server.js
+++ server.js
@@ -2,15 +2,22 @@
 var express = require('express');
 var path = require('path');
 const history = require('connect-history-api-fallback');
-var serveStatic = require('serve-static');
 app = express();
-const staticFileMiddleware = express.static(__dirname);
+var serveStatic = require('serve-static');
+app.use(serveStatic(__dirname + "/dist"));
+
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 app.use(staticFileMiddleware);
 app.use(history({
   disableDotRule: true,
   verbose: true,
 }));
 app.use(staticFileMiddleware);
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'));
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
