6d2fe8b87a2a8e151d184794b1424d69d4e7b00f
iasminaedina <iasmina.edina@gmail.com>
2019-07-11 14:38:33 +0200

    possible fix for heroku

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
-app.get('/*', (req, res) => res.send('/'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'App.vue'), function (err) {
+    if (err) {
+    res.status(500).send(err)
+    }
+  })
+})
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
