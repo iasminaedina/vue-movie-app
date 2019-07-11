71aeacce962b412201872807c770bba5f05f7610
iasminaedina <iasmina.edina@gmail.com>
2019-07-11 14:34:11 +0200

    possible fix for heroku

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
+app.get('/*', (req, res) => res.send('/'));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
